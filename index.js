2c943640e9db8092a30c466f4a76592cd2fd6167
Janka <jankadiscord@gmail.com>
2021-09-19 14:40:16 +0300

    first commands

--- index.js
+++ index.js
@@ -6,8 +6,18 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 client.once('ready', () => {
 	console.log('Bot is on...');
 });
-client.on('message', () => {
-	
-} )
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply('Server info.');
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+});
 
 client.login(token);
